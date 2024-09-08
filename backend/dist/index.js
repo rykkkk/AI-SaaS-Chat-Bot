bcee2adbb966fd6572a05378cb6d5c7b467c5b86
ryk-k <rykk@sheridancollege.ca>
2024-09-08 15:10:40 -0400

    first request

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,18 +1,18 @@
-import express from 'express'
-
+import express from 'express';
 const app = express();
+
+
 
 //routes/ endpoints for the requests
 //app.httprequest("/routepagename", callbackfunction(requestobject, response, next))
 // GET
 app.get("/hello", (req, res, next) => {
-  //send somehting to client in return statement
-  return res.send("string we sent = hello"); 
+    //send somehting to client in return statement
+    return res.send("string we sent = hello");
 });
 // PUT - update/modify data
 // POST - new data
 // DELETE
-
-
 //open dev portal - localhost
-app.listen(5000,()=>console.log("Server Open"));
+app.listen(5000, () => console.log("Server Open"));
+//# sourceMappingURL=index.js.map
